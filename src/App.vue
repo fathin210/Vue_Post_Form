ed175ac104b278bd782bbd4f918c443f4eebb13a
Fathin <fathin210@gmail.com>
2021-05-17 08:32:01 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Belajar submit form dengan Vue Js</h1>
+
+    <PostForm />
+  </div>
+</template>
+
+<script>
+import PostForm from "./components/PostForm";
+
+export default {
+  name: "App",
+  components: {
+    PostForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <form class="wrap" @submit.prevent="submit">
+    <input type="text" v-model="mahasiswa.nama" placeholder="Nama" />
+    <input type="text" v-model="mahasiswa.nim" placeholder="NIM" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "PostForm",
+  data() {
+    return {
+      mahasiswa: {
+        nama: "",
+        nim: "",
+      },
+    };
+  },
+
+  methods: {
+    submit() {
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          nama: this.mahasiswa.nama,
+          nim: this.mahasiswa.nim,
+        })
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+
+      // Ini kalo gunain fetch
+      //   fetch("https://jsonplaceholder.typicode.com/posts", {
+      //     method: "POST",
+      //     headers: { "Content-Type": "application/json" },
+      //     body: JSON.stringify({
+      //       nama: this.mahasiswa.nama,
+      //       nim: this.mahasiswa.nim,
+      //     }),
+      //   })
+      //     .then((res) => console.log(res.json()))
+      //     .catch((err) => console.log(err));
+      // },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 20px;
+}
+
+input {
+  margin-bottom: 10px;
+  padding: 10px;
+}
+
+button {
+  padding: 10px;
+  background-color: midnightblue;
+  color: white;
+}
+</style>
